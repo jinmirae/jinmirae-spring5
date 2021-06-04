6fc90025da2bc0790aaed3d5acc9519f97e1c963
jinmirae <mirae583@naver.com>
2021-06-04 15:26:14 +0900

    5교시 커밋

--- DATABASES/SQL진도_20210604.sql
+++ DATABASES/SQL진도_20210604.sql
@@ -1,3 +1,26 @@
+--8장 함수(count,upper,lower,to_char,round...) 그룹함수
+--부서별 연봉의 합계를 구해서 제일 급여가 많이 지출되는 부서(아래)
+--자바코딩에서는 소문자로 통일합니다.Select
+--DB셋팅에서 대소문자구분해서 사용할지, 구분하지 않을 지 셋팅
+SELECT R.deptno, R.dept_sal FROM (
+SELECT deptno, sum(sal) AS dept_sal 
+FROM emp GROUP BY deptno
+) R ORDER BY dept_sal DESC;--R의 역할은 Alias별명입니다.
+SELECT deptno, sum(sal) FROM emp
+GROUP BY deptno
+ORDER BY SUM(sal) DESC;
+--라우드함수(반올림) 소수점기준. round(10.05,2)소수점2째반올림
+SELECT ename, round(sal) FROM emp;--레코드가 여러개
+SELECT sum(sal) FROM emp;--1개의 레코드만 그룹함수라고 말함.
+SELECT round(AVG(sal)) FROM emp;--평균 1개의 레코드로 출력
+SELECT COUNT(*) FROM emp WHERE sal < 
+(SELECT round(AVG(sal)) FROM emp);
+--위쿼리는 사원중에 평균연봉보다 많이 받는 사람의 숫자.error
+--위 AVG함수를 Where조건에 사용못할때 서브쿼리를 이용합니다.
+SELECT MAX(sal)
+, MIN(sal)
+, MAX(sal)-MIN(sal) AS "대표와사원의연봉차"
+FROM emp;
 --DDL문(create; alter;), DCL문(commit; rollback;)
 --DML문(Dara Manufacture Language) insert, update,delete
 --inser문:테이블에 새로운 레코드(row)를 추가
@@ -12,6 +35,7 @@ deptno, dname, loc
 10, '개발부서', '천안'
 --바인딩값
 );
+select * from dept order by deptno;
 insert into dept02 values(20,'디자인부','경기도');
 --DCL명령어 
 COMMIT;--데이터베이스쿼리직접입력한 결과는 반드시 커밋을 해줘야지만 
@@ -25,8 +49,11 @@ DROP TABLE dept02;--드롭 테이블 커밋없이 바로 적용가능.
 CREATE TABLE emp01 AS SELECT * FROM emp;--테이블복제명령
 SELECT * FROM emp01;
 --UPDATE TABLE명 SET 필드명 = '바꿀값' where empno='특정ID'
-UPDATE emp01 SET ename = '진미래';
+UPDATE emp01 SET ename = '구꽁이' WHERE empno = 7839;
 ROLLBACK;--롤백은 마지막 커밋 바로전까지 되돌립니다.
+UPDATE emp01 SET sal = sal*1.1;--모든직원연봉을 10%인상
+UPDATE emp01 SET hiredate = sysdate;
+--머지 표준쿼리(ANSI)가 아니라서 건너뜀
 
 
 
