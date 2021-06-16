2e4894eebdc0a4b136465a61afd0d7a984baf5e3
jinmirae <mirae583@naver.com>
2021-06-16 20:47:08 +0900

    message

---
+++ src/main/java/com/edu/vo/BoardTypeVO.java
@@ -0,0 +1,31 @@
+package com.edu.vo;
+/**
+ * 이 클래스는 게시판생성관리의 데이터를 입/출력
+ * @author Gukko
+ *
+ */
+public class BoardTypeVO {
+	//멤버변수 생성
+	private String board_type;//PK고유값, 식별자
+	private String board_name;
+	private Integer board_sun;//int>Integer null을 허용 nullPoint
+	public String getBoard_type() {
+		return board_type;
+	}
+	public void setBoard_type(String board_type) {
+		this.board_type = board_type;
+	}
+	public String getBoard_name() {
+		return board_name;
+	}
+	public void setBoard_name(String board_name) {
+		this.board_name = board_name;
+	}
+	public Integer getBoard_sun() {
+		return board_sun;
+	}
+	public void setBoard_sun(Integer board_sun) {
+		this.board_sun = board_sun;
+	}
+	
+}
