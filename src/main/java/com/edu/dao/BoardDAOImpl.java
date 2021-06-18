aa686621b425d94d6a464384bc752a8e347fb62f
jinmirae <mirae583@naver.com>
2021-06-18 17:36:29 +0900

    8교시 커밋

--- src/main/java/com/edu/dao/BoardDAOImpl.java
+++ src/main/java/com/edu/dao/BoardDAOImpl.java
@@ -5,50 +5,97 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import com.edu.vo.BoardTypeVO;
+import com.edu.vo.AttachVO;
+import com.edu.vo.BoardVO;
+import com.edu.vo.PageVO;
 
 /**
- * 이 클래스는 게시판생성관리 쿼리의 인터페이스를 구현메서드가 있는 클래스 입니다.
+ * 이 클래스는 게시물 CRUD를 구현하는 DAO클래스 입니다.
  * @author 진미래
  *
  */
 @Repository
-public class BoardTypeDAOImpl implements IF_BoardTypeDAO {
-	//sqlSession템플릿(틀) 의존성을 주입
-	@Inject //자바8부터 신규로 나온 애노테이션 입니다.
-	private SqlSession sqlSession;
+public class BoardDAOImpl implements IF_BoardDAO {
+	private Logger logger = LoggerFactory.getLogger(BoardDAOImpl.class);
+	@Inject
+	private SqlSession sqlSession;//sql세션템플릿(틀)에는 insert,update()...
 	
 	@Override
-	public void deleteBoardType(String board_type) throws Exception {
-		// TODO sqlSession템플릿(틀)을 이용해서 매퍼쿼를 실행
-		sqlSession.delete("boardTypeMapper.deleteBoardType", board_type);
-		// 서식 sqlSession.~템플릿메서드("SQL쿼리위치", 테이터객체변수);
+	public void deleteAttachAll(Integer bno) throws Exception {
+		// TODO sql세션템플릿을 상용한 매퍼쿼리 호출(아래)
+		sqlSession.delete("boardMapper.deleteAttachAll", bno);
 	}
 
 	@Override
-	public void updateBoardType(BoardTypeVO boardTypeVO) throws Exception {
+	public void deleteAttach(String save_file_name) throws Exception {
+		// TODO 위 주석과 동일
+		sqlSession.delete("boardMapper.deleteAttach", save_file_name);
+	}
+
+	@Override
+	public void updateAttach(AttachVO attachVO) throws Exception {
+		// TODO 아래 주석 동일-첨부파일 주, 메서드명은 update지만, 쿼리는 insert
+		sqlSession.insert("boardMapper.updateAttach", attachVO);
+	}
+
+	@Override
+	public void insertAttach(AttachVO attachVO) throws Exception {
+		// TODO 아래 주석 동일-첨부파일
+		sqlSession.insert("boardMapper.insertAttach", attachVO);
+	}
+
+	@Override
+	public List<AttachVO> readAttach(Integer bno) throws Exception {
+		// TODO 아래 주석 동일-첨부파일
+		return sqlSession.selectList("boardMapper.readAttach", bno);
+	}
+
+	@Override
+	public void updateViewCount(Integer bno) throws Exception {
 		// TODO 아래 주석 동일
-		sqlSession.update("boardTypeMapper.updateBoardType", boardTypeVO);
+		sqlSession.update("boardMapper.updateViewCount", bno);
 	}
 
 	@Override
-	public BoardTypeVO readBoardType(String board_type) throws Exception {
+	public int countBoard(PageVO pageVO) throws Exception {
 		// TODO 아래 주석 동일
-		return sqlSession.selectOne("boardTypeMapper.readBoardType", board_type);
+		return sqlSession.selectOne("boardMapper.countBoard", pageVO);
 	}
 
 	@Override
-	public void insertBoardType(BoardTypeVO boardTypeVO) throws Exception {
+	public void deleteBoard(int bno) throws Exception {
 		// TODO 아래 주석 동일
-		sqlSession.insert("boardTypeMapper.insertBoardType", boardTypeVO);
+		sqlSession.delete("boardMapper.deleteBoard", bno);
 	}
 
 	@Override
-	public List<BoardTypeVO> selectBoardType() throws Exception {
-		// TODO 매퍼쿼리를 실행 sqlSession템플릿(틀)을 이용해서
-		return sqlSession.selectList("boardTypeMapper.selectBoardType");
+	public void updateBoard(BoardVO boardVO) throws Exception {
+		// TODO 아래 주석 동일
+		sqlSession.update("boardMapper.updateBoard", boardVO);
+	}
+
+	@Override
+	public BoardVO readBoard(int bno) throws Exception {
+		// TODO 아래 주석 동일
+		return sqlSession.selectOne("boardMapper.readBoard", bno);
+	}
+
+	@Override
+	public int insertBoard(BoardVO boardVO) throws Exception {
+		// TODO 아래 주석 동일+ 게시물 입력 후 반환값으로 bno를 받습니다.
+		sqlSession.insert("boardMapper.insertBoard", boardVO);
+		logger.info("디버그: " + boardVO.getBno());
+		return boardVO.getBno();
+	}
+
+	@Override
+	public List<BoardVO> selectBoard(PageVO pageVO) throws Exception {
+		// TODO sqlSession템플릿을 사용해서 매퍼쿼리 실행
+		return sqlSession.selectList("boardMapper.selectBoard", pageVO);
 	}
 
 }
--- src/main/java/com/edu/dao/IF_BoardDAO.java
+++ src/main/java/com/edu/dao/IF_BoardDAO.java
@@ -7,9 +7,10 @@ import com.edu.vo.BoardVO;
 import com.edu.vo.PageVO;
 
 /**
- * 이 클래스는 boardMapper.xml을 접근하기 위한 DataAccessObject 입니다.
+ * 이 인터페이스는 boardMapper.xml을 접근하기 위한 DataAccessObject 입니다.
  * DAO 와 서비스를 나눈 이유 확인.
  * @author 진미래
+ *
  */
 public interface IF_BoardDAO {
 	//첨부파일 CRUD 아래
@@ -18,14 +19,14 @@ public interface IF_BoardDAO {
 	public void updateAttach(AttachVO attachVO) throws Exception;
 	public void insertAttach(AttachVO attachVO) throws Exception;
 	public List<AttachVO> readAttach(Integer bno) throws Exception;
-	//페이징에서 사용할 totalCount변수값을 구하는 메서드(아래)
-	public void updateViweCount(Integer bno) throws Exception;
-	//페이징 없는 검색된 (board_type포함)게시물 개수
+	//게시물 상세보기시 조회수 올리는 메서드(아래)
+	public void updateViewCount(Integer bno) throws Exception;
+	//페이징 없는 검색된(board_type포함된) 게시물 개수
 	public int countBoard(PageVO pageVO) throws Exception;
 	//기본 CRUD 아래
-	public BoardVO deleteBoard(int bno) throws Exception;
+	public void deleteBoard(int bno) throws Exception;
 	public void updateBoard(BoardVO boardVO) throws Exception;
 	public BoardVO readBoard(int bno) throws Exception;
-	public void insertBoard(BoardVO boardVO) throws Exception;
+	public int insertBoard(BoardVO boardVO) throws Exception;
 	public List<BoardVO> selectBoard(PageVO pageVO) throws Exception;
 }
